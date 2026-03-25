72e6a92878584694812ab948040bcd35bc380c65
Hamed Abdul-Azeez <zeezdesign123@gmail.com>
2026-03-25 13:42:43 +0100

    start functionality created, Ui updated (buttons and timer display)

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-// import {format, formatDistance } from 'https://cdn.jsdelivr.net/npm/date-fns@3.6.0/+esm';
+// import {format, formatDistance } from 'https://cdn.jsdelivr.net/npm/Date-fns@3.6.0/+esm';
 
 const clock = document.querySelector('.clock');
+const start = document.querySelector('.start-bttn');
+const stop = document.querySelector('.stop-bttn');
+const pause = document.querySelector('.pause-bttn');
 
-timer = ()=>{
-    const now = new Date();
-    const hours = now.getHours();
-    const mins = now.getMinutes();
-    const sec = now.getSeconds();
-    const Msec = now.getMilliseconds();
+const elapsedtime = ()=>{
+    const startTime = Date.now();
+    setInterval(()=>{
+        const currentTime = Date.now();
+        let elapsed = currentTime - startTime;
 
-    const html = `
-        <span>${hours}:</span> 
-        <span>${mins}:</span> 
-        <span>${sec}:</span>
-        <span>${Msec}:</span> 
-    `;
+        const hour = Math.floor(elapsed / (1000 * 60 * 60));
+        const min = Math.floor((elapsed / (1000 * 60)) % 60);
+        const sec = Math.floor((elapsed / 1000) % 60);
+        const MillSec = elapsed % 100;
 
-    clock.innerHTML = html
+        const format = (num, size) => String(num).padStart(size, '0');
+        const html = `
+        <span>${format(hour, 2)}</span> 
+        <span>${format(min, 2)}</span> 
+        <span>${format(sec, 2)}</span>
+        <span>${format(MillSec, 2)}</span>
+        `
+        
+        clock.innerHTML = html
+    }, 50);
 }
 
-setInterval(timer, 1000)
-
+start.addEventListener('click', elapsedtime);
