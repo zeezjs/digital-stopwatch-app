85c805abc6b8439093281bebc6512849cf366936
Hamed Abdul-Azeez <zeezdesign123@gmail.com>
2026-03-26 11:18:34 +0100

    Perfected states and understood stateManagement

--- app.js
+++ app.js
@@ -4,37 +4,62 @@ const pauseBtn = document.querySelector('.pause-bttn');
 const stopBtn = document.querySelector('.stop-bttn');
 
 //states
-let elapsedtime = 0;
-let startTime = 0;
-let isRunning = false;
-let intervalid = null;
+let startTime = 0; //time user clicked start
+let elapsedTime = 0;//total time covered (meausered) since user clicked start
+let interValid = null;//variable for our interval (loop)
+let isRunning = false; //the state of the stopwatch
 
 //functions
+//format function
+const format = (num, size) => String(num).padStart(size, '0');
 
-const update = ()=>{
-        const hour = Math.floor(elapsedtime / (1000 * 60 * 60));
-        const min = Math.floor((elapsedtime / (1000 * 60)) % 60);
-        const sec = Math.floor((elapsedtime / 1000) % 60);
-        const MillSec = elapsedtime % 1000;
+//Display function
+const updateClock = ()=>{
+    const hours = Math.floor(elapsedTime/(1000*60*60));
+    const mins = Math.floor(elapsedTime/(1000*60)%60);
+    const secs = Math.floor(elapsedTime/(1000)%60);
+    const milliSecs = elapsedTime % 1000;
 
-        const format = (num, size) => String(num).padStart(size, '0');
-        const html = `
-        <span>${format(hour, 2)}</span> 
-        <span>${format(min, 2)}</span> 
-        <span>${format(sec, 2)}</span>
-        <span>${format(MillSec, 2)}</span>
-        `
-        clock.innerHTML = html
-    }//update display function
+    const html = `
+        <span>${format(hours, 2)}</span>
+        <span>${format(mins, 2)}</span>
+        <span>${format(secs, 2)}</span>
+        <span>${format(milliSecs, 3)}</span>
+    `
+
+    clock.innerHTML = html
+};
 
 //start
 const start = ()=>{
-    startTime = Date.now()
-    intervalid = setInterval(()=>{
-        elapsedtime = Date.now() - startTime
-        update();
-    }, 50)
+    if(isRunning)        
+    return;
+        isRunning = true;
+        startTime = Date.now();
+        interValid = setInterval(()=>{
+            elapsedTime = Date.now() - startTime;
+           updateClock();
+        }, 50)
+    };
+
+//pause function
+const pause = ()=>{
+    if(isRunning){
+        clearInterval(interValid);
+        isRunning = false;
+    }
 }
 
-startBtn.addEventListener('click', start);
+//stop function
+const stop = ()=>{
+    clearInterval(interValid);
+    isRunning = false;
+    startTime = 0;
+    elapsedTime = 0;
+    updateClock()
+};
 
+//Events
+startBtn.addEventListener('click', start);
+pauseBtn.addEventListener('click', pause);
+stopBtn.addEventListener('click', stop);
